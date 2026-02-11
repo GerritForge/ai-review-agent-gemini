8019eb9557e3231390adf8881cbb36c01f6fc0f1
Luca Milanesio <luca.milanesio@gmail.com>
2026-02-11 23:11:20 +0000

    Add correct license in JS and TS files
    Change-Id: I36b33e31c52b6f90dfa3f905f525dbf25a091365

--- web/index.ts
+++ web/index.ts
@@ -1,13 +1,16 @@
-// Copyright (C) 2026 GerritForge, Inc.
-//
-// Licensed under the BSL 1.1 (the "License");
-// you may not use this file except in compliance with the License.
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/**
+ * @license
+ * Copyright (C) 2026 GerritForge, Inc.
+ *
+ * Licensed under the BSL 1.1 (the "License");
+ * you may not use this file except in compliance with the License.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 import '@gerritcodereview/typescript-api/gerrit';
 import type {PluginApi} from '@gerritcodereview/typescript-api/plugin';
